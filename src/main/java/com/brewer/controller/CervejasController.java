4fadb606ca0a48b56c8941af0a864a8eec9237bf
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-10-04 11:20:25 -0300

    Trocado o log do spring pelo log4j e slf4j.

--- src/main/java/com/brewer/controller/CervejasController.java
+++ src/main/java/com/brewer/controller/CervejasController.java
@@ -2,6 +2,8 @@ package com.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,9 +15,14 @@ import com.brewer.model.Cerveja;
 
 @Controller
 public class CervejasController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
 
 	@RequestMapping("cervejas/novo")
 	public String novo(Cerveja cerveja) {
+		logger.error("Aqui é um log nível error");
+		logger.info("Aqui é um log nível info");
+		
 		return "cerveja/CadastroCerveja";
 	}
 	
