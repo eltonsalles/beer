0c1cdb4a60d8349938c1cb5166033e7ce71a0f53
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-10-08 18:37:06 -0300

    Modularização do javascript

--- target/classes/static/javascripts/brewer.js
+++ target/classes/static/javascripts/brewer.js
@@ -1,7 +1,21 @@
-$(function() {
-	var decimal = $('.js-decimal');
-	decimal.maskMoney({decimal: ',', thousands: '.'});
+var Brewer = Brewer || {};
+
+Brewer.MaskMoney = (function() {
+	// Construtor
+	function MaskMoney() {
+		this.decimal = $('.js-decimal');
+		this.int = $('.js-int');
+	}
 	
-	var int = $('.js-int');
-	int.maskMoney({precision: 0, thousands: '.'});
+	MaskMoney.prototype.enable = function() {
+		this.decimal.maskMoney({decimal: ',', thousands: '.'});
+		this.int.maskMoney({precision: 0, thousands: '.'});
+	}
+	
+	return MaskMoney;
+})();
+
+$(function() {
+	var maskMoney = new Brewer.MaskMoney();
+	maskMoney.enable();
 });
